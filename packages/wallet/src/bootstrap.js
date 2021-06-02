fc18d4f173e01ed9a0385f58e116875d014fba9e
TNSahid <sahid.miller@gmail.com>
2021-06-02 15:15:08 -0400

    fix: correct import

--- packages/wallet/src/bootstrap.js
+++ packages/wallet/src/bootstrap.js
@@ -1,6 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { h, render } from "preact";
-import App from "./components/App";
+import AppRouter from "./components/App";
 
-render(<App />, document.querySelector("#app"));
+render(<AppRouter />, document.querySelector("#app"));
