81a9e30c7564edb1e14be5f5e393ac29baae2abb
TNSahid <sahid.miller@gmail.com>
2021-06-02 20:44:20 -0400

    fix: appRouter named export

--- packages/wallet/src/bootstrap.js
+++ packages/wallet/src/bootstrap.js
@@ -1,6 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { h, render } from "preact";
-import AppRouter from "./components/App";
+import { AppRouter } from "./components/App";
 
 render(<AppRouter />, document.querySelector("#app"));
