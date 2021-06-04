64ac494d0afd7a93144fdfadc150e0a139549801
TNSahid <sahid.miller@gmail.com>
2021-06-04 10:57:27 -0400

    fix: error fetching ipns on production
    due to signup_wallet_entry_path env variable empty on production.
    adds error handling

--- packages/shell/src/hooks/useGatewayIpns.js
+++ packages/shell/src/hooks/useGatewayIpns.js
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "preact/hooks";
+import { toast } from "react-toastify";
 
 import fetchIpns from "../utils/fetchIpns";
 
 export default function useGatewayIpns(ipnsAddress) {
   const [latestIpfsPath, setLatestIpfsPath] = useState(null);
+  const [hasErrored, setHasErrored] = useState(false);
 
   async function checkForIpfsUpdates() {
-    const latestAvailableSignupIpfsPath = await fetchIpns(ipnsAddress, false);
+    try {
+      const latestAvailableSignupIpfsPath = await fetchIpns(ipnsAddress, false);
 
-    if (latestAvailableSignupIpfsPath) {
-      setLatestIpfsPath(latestAvailableSignupIpfsPath);
+      if (latestAvailableSignupIpfsPath) {
+        setLatestIpfsPath(latestAvailableSignupIpfsPath);
+      }
+    } catch (ex) {
+      if (!hasErrored) {
+        console.log(ex);
+        toast.error("Error checking for updates!");
+        setHasErrored(true);
+      }
     }
   }
 
--- packages/shell/src/utils/fetchIpns.js
+++ packages/shell/src/utils/fetchIpns.js
@@ -1,14 +1,22 @@
-import { IPFS_GATEWAY_HOST, SIGNUP_WALLET_ENTRY_HOST } from '../config'
+import {
+  isIpfsEnv,
+  IPFS_GATEWAY_HOST,
+  SIGNUP_WALLET_ENTRY_HOST,
+} from "../config";
 
-const RESOLVE_IPNS_API_URL = "/api/v0/name/resolve"
+const RESOLVE_IPNS_API_URL = "/api/v0/name/resolve";
 
 export default async function resolveLatestIpns(address, nocache) {
-  if (process.env.NODE_ENV === "development") {
-    return SIGNUP_WALLET_ENTRY_HOST
+  if (!isIpfsEnv) {
+    return SIGNUP_WALLET_ENTRY_HOST;
   }
 
-  const response = await fetch(IPFS_GATEWAY_HOST + RESOLVE_IPNS_API_URL + `?arg=${address}${!!nocache ? '&nocache=true' : ''}`)
-  const { Path } = await response.json()
-  
-  return path && IPFS_GATEWAY_HOST + Path
+  const response = await fetch(
+    IPFS_GATEWAY_HOST +
+      RESOLVE_IPNS_API_URL +
+      `?arg=${address}${!!nocache ? "&nocache=true" : ""}`
+  );
+  const { Path } = await response.json();
+
+  return path && IPFS_GATEWAY_HOST + Path;
 }
